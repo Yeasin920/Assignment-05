675719e265b1d16519dddd2e8bc8fe8bb4706896
Yeasin920 <yeasinhafez420@gmail.com>
2025-08-31 11:18:39 +0600

    first upgraded index.js

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-function getElement(id) {
-  const element = document.getElementById(id);
-  return element;
-}
-document.getElementsByClassName("hrt-btn").addEventListener("click",function(e){
-    const hrt = document.getElementById("hrt-cnt").innerText
-    for(let hrts of hrt){
-        return hrts+= 1;
-    }
-
-
-})
 const heartCounter = document.getElementById("hrt-cnt");
-let count = 0;
-
-// get all heart icons
 const heartBtns = document.querySelectorAll(".hrt-btn");
-
-// add event listener for each heart
-heartBtns.forEach(btn => {
+for (const btn of heartBtns) {
   btn.addEventListener("click", () => {
-    count++;
-    heartCounter.textContent = count;
+    heartCounter.textContent = parseInt(heartCounter.textContent) + 1;
   });
-});
+}
