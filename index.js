a785b7a9b32cf7f1a860f6b3e99e7e43a3270518
Yeasin920 <yeasinhafez420@gmail.com>
2025-08-31 12:34:05 +0600

    complete html and css

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const heartCounter = document.getElementById("hrt-cnt");
 const heartBtns = document.querySelectorAll(".hrt-btn");
 for (const btn of heartBtns) {
-  btn.addEventListener("click", () => {
+  btn.addEventListener("click", function(){
     heartCounter.textContent = parseInt(heartCounter.textContent) + 1;
   });
 }
+
